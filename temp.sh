f24ee50148eaffdad4e44c82f2b150dd23c53b1e
meme <meme>
2025-12-22 20:42:41 +0800

    111

--- temp.sh
+++ temp.sh
@@ -1,23 +1,12 @@
 #!/usr/bin/env bash
 
-kubectl create namespace bloginfer
-
-kubectl apply -f configs/pv_pvc.yaml
-
-kubectl apply -f configs/train.yaml
-
-kubectl apply -f configs/integrajob.yaml
-
 sudo buildkitd --oci-worker=false --containerd-worker=true
 
+wrk -t4 -c200 -d60s http://8.216.0.230:30080
 
-kubectl create configmap mysql-init --from-file=init.sql=configs/init.sql -n bloginfer
+kubectl get pods -n bloginfer
 
-kubectl apply -f configs/redis.yaml
-kubectl apply -f configs/blog.yaml
-kubectl apply -f configs/mysql.yaml
-kubectl apply -f configs/inference.yaml
-kubectl apply -f configs/nginx.yaml
+kubectl logs deploy/train-daemon -n bloginfer
 
 kubectl get svc redis blog-backend-service
 sudo nerdctl -n k8s.io rmi -f my_repository/blog-backend:latest

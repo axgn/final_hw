7bb683b05be48621c7d7fcce0cc4b03ad344ce01
meme <meme>
2025-12-22 21:11:40 +0800

    111

--- temp.sh
+++ temp.sh
@@ -8,6 +8,15 @@ kubectl get pods -n bloginfer
 
 kubectl logs deploy/train-daemon -n bloginfer
 
+kubectl exec -it mysql-0 -n bloginfer -- bash
+
+show databases;
+use blog;
+show tables;
+select * from users;
+
+
+
 kubectl get svc redis blog-backend-service
 sudo nerdctl -n k8s.io rmi -f my_repository/blog-backend:latest
 kubectl describe pod blog-backend-deployment-795765df44-d6vgx -n bloginfer

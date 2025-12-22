473e256bade9110126d6b64336b74a236ccc7ac4
meme <meme>
2025-12-22 19:00:33 +0800

    修改关机文件

--- shutdown.sh
+++ shutdown.sh
@@ -5,6 +5,8 @@ kubectl delete -f configs/blog.yaml
 kubectl delete -f configs/mysql.yaml
 kubectl delete -f configs/inference.yaml
 kubectl delete -f configs/nginx.yaml
+kubectl delete -f configs/train-daemon.yaml
 rm -rf ../sql_data
 
 rm -f /root/final_hw/models/client_*.pt 2>/dev/null || true
+
